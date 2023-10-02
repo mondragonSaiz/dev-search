959be8e2963aa919e3c98486930915405c7f6ca5
David Mondragon <dragon18saiz@gmail.com>
2023-10-01 22:47:46 -0600

    feat: productList rendered in projects/project templates

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList = [
+    {
+        'id': '1',
+        'title': "RoomieHunt",
+        'description': 'Web app to find your best match to live with!'
+    },
+      {
+        'id': '2',
+        'title': "GoalHub",
+        'description': 'All your teamwork activities, progress and more in once place!'
+    },
+      {
+        'id': '3',
+        'title': "Fridge2Table",
+        'description': 'Grab your availble groceries and start creating awesome recipes!'
+    },
+]
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = 'Hello, you are on the projects page'
+    return render(request, 'projects/projects.html', {'projectList': projectList})
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    project = None
+    for i in projectList:
+        if i['id'] == pk :
+            project= i
+    return render(request, 'projects/single-project.html', {'project': project})
