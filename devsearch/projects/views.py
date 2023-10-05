d5b8c2e5ba65f787b736477ef826893d9f88aeeb
David Mondragon <dragon18saiz@gmail.com>
2023-10-04 21:44:43 -0600

    refactor(views.py): deleted mock project list

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,23 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project, Review, Tag
-projectList = [
-    {
-        'id': '1',
-        'title': "RoomieHunt",
-        'description': 'Web app to find your best match to live with!'
-    },
-      {
-        'id': '2',
-        'title': "GoalHub",
-        'description': 'All your teamwork activities, progress and more in once place!'
-    },
-      {
-        'id': '3',
-        'title': "Fridge2Table",
-        'description': 'Grab your availble groceries and start creating awesome recipes!'
-    },
-]
+
 def projects(request):
     projects = Project.objects.all()
     context = {'projects': projects}
