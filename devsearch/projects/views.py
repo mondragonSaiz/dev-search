78fa7d80e1980f65c515e0f2876768c78d4314f2
David Mondragon <dragon18saiz@gmail.com>
2023-10-11 10:59:58 -0600

    feat(views.py): Post request recieved in create_project view

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project, Review, Tag
 from .forms import ProjectForm
@@ -17,5 +17,11 @@ def project(request, pk):
 
 def create_project(request):
     form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
     context ={'form': form}
     return render(request, "projects/project_form.html", context)
