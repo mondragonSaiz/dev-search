fe620b755c43e763e03810419df5157653c96c65
David Mondragon <dragon18saiz@gmail.com>
2023-10-12 16:18:57 -0600

    feat(upload image to project funtion): featured_image field added to project model, media root added to settings.py, form modified to recive files, create_project and update_project views modified to recieve request.FILES

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -1,6 +1,9 @@
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -8,3 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
